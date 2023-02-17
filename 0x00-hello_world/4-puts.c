b32dd30776ee2b7f71758e23432d5d8d267284b3
emmaolondo <emmaolondo448@gmail.com>
2023-02-17 07:45:45 -0800

    using puts with betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -using the puts function to display the output of a string
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
