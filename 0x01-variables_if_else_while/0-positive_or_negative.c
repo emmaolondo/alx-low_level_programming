d5d470e748311f24a1add03a58bc9ca82fb39c5f
emmaolondo <emmaolondo448@gmail.com>
2023-02-17 15:46:02 -0800

    comparison of a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - comparing the size of an integer when a random number is added
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d\t %s", n,"is positive");
+}
+else if (n == 0)
+{
+printf("%d\t %s", n, "is zero");
+}
+else if (n < 0)
+{
+printf("%d\t %s", n, "is negative");
+}
+return (0);
+}
