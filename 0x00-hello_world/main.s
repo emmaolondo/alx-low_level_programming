219ac47513665922ba10b6025186857ccd4019af
emmaolondo <emmaolondo448@gmail.com>
2023-02-17 06:05:21 -0800

    A c Assember file

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -3,20 +3,13 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB23:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE23:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
