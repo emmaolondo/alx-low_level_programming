cbbef58fc2db625a1816dc921eee23cf5f079a1c
emmaolondo <emmaolondo448@gmail.com>
2023-02-17 13:43:29 -0800

    Assembler code as output

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -3,13 +3,20 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB23:
+.LFB0:
 	.cfi_startproc
 	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
 	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE23:
+.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
