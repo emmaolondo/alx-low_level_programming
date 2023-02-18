e298cf12e07ebf83413545d6d2c225934bb77412
emmaolondo <emmaolondo448@gmail.com>
2023-02-17 23:51:47 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single numbers starting from 0
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
