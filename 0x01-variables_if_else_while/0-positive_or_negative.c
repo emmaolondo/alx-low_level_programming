b8b120f04ca236ee72b468d3407596c891cfd435
emmaolondo <emmaolondo448@gmail.com>
2023-02-18 07:23:09 -0800

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,8 @@
 int main(void)
 {
 int n;
-n = rand();
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
---
+++ 0x01-variables_if_else_while/trial.c
@@ -0,0 +1,12 @@
+/**
+ * main - some description
+ *
+ *return: 0
+ */
+
+or
+/**
+ *main - Entrypoint
+ *description
+ *return: 0
+ */
