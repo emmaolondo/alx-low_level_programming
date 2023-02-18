6aacda5fb826d30802d0786355aa2f71930dadfa
emmaolondo <emmaolondo448@gmail.com>
2023-02-18 07:40:59 -0800

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
