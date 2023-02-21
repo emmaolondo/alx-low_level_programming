790aeb52fdc481c53ad157ecf1a4f2c72822df2a
emmaolondo <emmaolondo448@gmail.com>
2023-02-21 14:46:00 -0800

    the start

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_putchar - Display the charater in a function
+ *@ch: local variable
+ *
+ *Return: 0 (success)
+ */
+int _putchar(int ch)
+{
+putchar(ch);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Working with c functions, nested loops
