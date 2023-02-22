2fc80382977c8a4c5480a13dbf43e45cabdc46d4
emmaolondo <emmaolondo448@gmail.com>
2023-02-21 16:35:45 -0800

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 int ch;
 int row_counts;
-for (row_counts  0; row_counts <= 10; row_counts++)
+for (row_counts = 0; row_counts <= 10; row_counts++)
 {
-for (ch = 'a'; ch = 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar(ch);
 }
