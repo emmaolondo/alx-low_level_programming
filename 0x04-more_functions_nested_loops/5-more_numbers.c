12fd89996e808fb9672d01828042f0846d9df8ae
emmaolondo <emmaolondo448@gmail.com>
2023-02-23 13:14:45 -0800

    print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,3 +5,17 @@
  *
  *Return: 0
  */
+void more_numbers(void)
+{
+int row_count = 1;
+int n;
+while (row_count <= 10)
+{
+for (n = 0; n <= 14; n++)
+{
+_putchar(n);
+_putchar('\n');
+}
+row_count++;
+}
+}
