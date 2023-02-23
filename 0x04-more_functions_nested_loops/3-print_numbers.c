10eb3f8117d27c137491cf1893236d42c56aa7a1
emmaolondo <emmaolondo448@gmail.com>
2023-02-23 12:03:18 -0800

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - print numbers
+ *
+ *Return: 0
+ */
+void print_numbers(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+return (0);
+}
