e38cdd8a8c241dcac6076630116d454525a136f3
emmaolondo <emmaolondo448@gmail.com>
2023-02-23 11:08:23 -0800

    product of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - find the product of two integers
+ *@a: Local variable
+ *@b: local variable
+ *
+ *Return: 0 (success)
+ */
+int mul(int a, int b)
+{
+int product = a * b;
+printf(product);
+return (0);
+}
