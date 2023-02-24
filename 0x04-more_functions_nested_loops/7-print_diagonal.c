82ff13b062103b64cf91c647fd289fec211439f8
emmaolondo <emmaolondo448@gmail.com>
2023-02-23 18:43:41 -0800

    diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,22 @@
 void print_diagonal(int n)
 {
 int spacecount;
+int k = 0;
 if (n <= 0)
 {
 _putchar('\n');
 }
 else
 {
+while (k <= n)
+{
 for (spacecount = 1; spacecount <= n; n++)
 {
 _putchar(' ');
+}
 _putchar('\\');
 _putchar('\n');
+k++;
+}
 }
 }
