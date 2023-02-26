a33242d222dc76136b4614877176a6f6cd93f9f5
emmaolondo <emmaolondo448@gmail.com>
2023-02-25 22:39:36 -0800

    print numbers upto 98'

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_to_98 - function that prints numbers upto 98
+ *@n: local variable
+ *
+ *Return: 0
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while(n < 98)
+printf("d, ", n++);
+printf("%d\n", n);
+}
+else if (n >= 98)
+{
+while (n > 98)
+{
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+}
+}
