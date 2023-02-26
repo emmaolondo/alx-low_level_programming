c2a9ef1dfe5c69048e157058736cbbec42557c3a
emmaolondo <emmaolondo448@gmail.com>
2023-02-25 22:46:03 -0800

    print numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-while(n < 98)
+while (n < 98)
 printf("%d, ", n++);
 printf("%d\n", n);
 }
@@ -20,7 +20,8 @@ else if (n >= 98)
 while (n > 98)
 {
 printf("%d, ", n--);
-printf("%d\n", n);
+printf("%d", n);
 }
+putchar('\n');
 }
 }
