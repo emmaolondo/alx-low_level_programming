d62833d20c07c4db2905772a5ab6601ccab13359
emmaolondo <emmaolondo448@gmail.com>
2023-02-27 14:15:21 -0800

    Reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+/**                                                                             
+ *_strlen - Outputs the length of the string                                    
+ *@s: a string pointer                                                          
+ *                                                                              
+ *Return: stringcount                                                           
+ */
+int _strlen(char *s)
+{
+int n = 0; /* string count */
+while (*s != '\0')
+{
+n++;
+s++;
+}
+return (n);
+}
+
 /**
  *print_rev - prints the string in a reverse order
  *@s: string pointer variable
