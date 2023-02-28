f73e99de2639a06fe01423506a027ef35a9d3380
emmaolondo <emmaolondo448@gmail.com>
2023-02-28 05:33:44 -0800

    print 14 integers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,16 @@ void more_numbers(void)
 {
 int row_count;
 int n;
- for (row_count = 0; row_count < 10; row_count++)
+for (row_count = 0; row_count < 10; row_count++)
 {
 for (n = 0; n <= 14; n++)
 {
 if (n > 9)
 {
 _putchar(n / 10 + '0');
-n = n % 10;
-_putchar((n + '0'));
+}
+_putchar((n % 10 + '0'));
 }
 _putchar('\n');
-}
 }
 }
