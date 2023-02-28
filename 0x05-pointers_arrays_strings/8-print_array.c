23bfe303f694c7ded22c7e6dee949065834c7a7f
emmaolondo <emmaolondo448@gmail.com>
2023-02-27 21:57:06 -0800

    print values of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_array - print values of an array
+ *@a: A pointer variable
+ *@n: array length
+ *
+ *
+ *Return: 0
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+}
