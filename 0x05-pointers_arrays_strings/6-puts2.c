4401d40b9b82ca49193119262b1d061cca5f2f83
emmaolondo <emmaolondo448@gmail.com>
2023-02-27 20:52:08 -0800

    print even numbers in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts2 - Prints the even numbers
+ *@str: pointer variable
+ *
+ *Return: 0
+ */
+void puts2(char *str)
+{
+int str_length, i;
+str_length = 0;
+i = 0;
+while (*str != '\0')
+{
+str++;
+str_length;
+}
+for (i = 0; i <= str_length - 2; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
