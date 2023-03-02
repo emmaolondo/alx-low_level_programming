657ec365559f7676b579c0d8b9994df1ffd33c54
emmaolondo <emmaolondo448@gmail.com>
2023-03-02 08:02:42 -0800

    Reverse an intger array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,14 +12,15 @@ void reverse_array(int *a, int n)
 {
 int p;
 int i = 0;
-int j = n -1;
+int j = n - 1;
 int *b, *c, tmp;
 b = a;/* points at the first element*/
 c = a; /*points at the first element*/
-for(p = 0; p < n - 1; p++)/* loop to move the c pointer at the end*/
-{c++; 
+for (p = 0; p < n - 1; p++)/* loop to move the c pointer at the end*/
+{
+c++;
 }
-for (p = 0; p < n / 2; p++) /* swap the pointers*/
+for (p = 0; p < n / 2; p++)/* swap the pointers*/
 {
 tmp = a[i];
 a[i] = b[j];
