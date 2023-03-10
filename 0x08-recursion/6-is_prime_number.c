74d99f0dd20fd99e8a396377ac243998dfbc3aa8
emmaolondo <emmaolondo448@gmail.com>
2023-03-09 17:55:01 -0800

    prime numbets

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *primenumber - checks if number is a prime number
+ *@a: integers variamble
+ *@b: integer variable
+ *
+ *
+ *return: integer
+ */
+int primenumber(int a, int b)
+{
+if (a == b)
+{
+return (1);
+}
+if (!(a % b))
+{
+return (0);
+}
+return (primenumber(a, b+1));  
+}
+
+/**
+ *is_prime_number - checks if a number is a prime number
+ *@n: integer value
+ *
+ *Returm:: 0
+ */
+int is_prime_number(int n)
+{
+if (n < 2)
+{
+return (0);
+}
+else
+return (primenumber(n, 2));
+}
