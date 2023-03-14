9501e8c41dcb3c9a69f5e3bf5177d8340c0a640c
emmaolondo <emmaolondo448@gmail.com>
2023-03-14 14:12:31 -0700

    own strdup()

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * strdup -  function that returns a
- * pointer to a newly allocated space in memory,
+ * _strdup -  function that returns a pointer to a
+ * newly allocated space in memory,
  * which contains a copy of the string given as a parameter
  * @str: string pointer
  *
@@ -24,7 +24,7 @@ while (str[n] != '\0')/* get the string length*/
 {
 n++;
 }
-ptr = (char *) malloc((n + 1) * sizeof(str));/* allocate memory for ptr*/
+ptr = (char *) malloc(sizeof(str) * (n + 1));/* allocate memory for ptr*/
 if (ptr == NULL)
 {
 return (NULL);
