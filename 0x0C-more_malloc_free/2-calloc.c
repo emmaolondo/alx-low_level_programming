288aac2f0cc49f213d087c23a66df833bbfdc415
emmaolondo <emmaolondo448@gmail.com>
2023-03-17 13:10:19 -0700

    implement calloc using malloc()

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,21 +12,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  char *ptr;
-  unsigned int i = 0;
-  if (nmemb == 0 || size == 0)
-    {
-      return (NULL);
-    }
-  ptr = malloc(nmemb * size);
-    if (ptr == NULL)
+char *ptr;
+unsigned int i = 0;
+if (nmemb == 0 || size == 0)
 {
-       return (NULL);
-      }
-  while (i < (nmemb * size))
-    {
-     ptr[i] = i;
-     i++;
-    }
-  return (ptr);
+return (NULL);
+}
+/* allocate the ptr to the memory*/
+ptr = (char *)malloc(nmemb * size);
+if (ptr == NULL)
+{/* check if ptr is empty*/
+return (NULL);
+}
+while (i < (nmemb * size))
+{/* loop to set mem to 0*/
+ptr[i] = 0;
+i++;
+}
+return (ptr);
 }
