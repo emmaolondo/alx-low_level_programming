67be512f1b0616851264c5eaffac49ed1a106c37
emmaolondo <emmaolondo448@gmail.com>
2023-03-22 09:34:36 +0300

    new dog corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,6 +22,7 @@ return (NULL);
 newdog = malloc(sizeof(dog_t));
 if (newdog == NULL)
 {
+free(newdog)
 return (NULL);
 }
 while (name[n] != '\0' && owner[o] != '\0')
@@ -31,8 +32,10 @@ o++;
 }
 newdog->name = malloc(sizeof(char) * n);
 newdog->owner = malloc(sizeof(char) * o);
-if (newdog->name == NULL || newdog->owner == NULL)
+if (newdog->name == NULL)
 {
+free(newdog->name)
+free(newdog)
 return (NULL);
 }
 for (i = 0; i <= n; i++)
@@ -40,6 +43,12 @@ for (i = 0; i <= n; i++)
 newdog->name[i] = name[i];
 }
 newdog->age = age;
+if (newdog->owner == NULL)
+{
+free(newdog->owner)
+free(newdog)
+return (NULL);
+}
 for (j = 0; j <= (o + 3); j++)
 {
 newdog->owner[j] = owner[j];
