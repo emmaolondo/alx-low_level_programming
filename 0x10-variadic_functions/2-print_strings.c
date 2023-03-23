7b688655d97dc4138490abaf91e45d47b8f2e729
emmaolondo <emmaolondo448@gmail.com>
2023-03-23 23:29:17 +0300

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,6 +15,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list list;
 char *str;
+if (separator == NULL)
+{
+separator = "";
+}
 va_start(list, n);
 for (i = 0; i < n; i++)
 {
