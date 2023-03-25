52fe5abefac4700646c51d015e86d29d30da1893
emmaolondo <emmaolondo448@gmail.com>
2023-03-25 06:03:46 +0300

    arithmetic operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - main function
+ *argc: argument count
+ *argv: argument vector
+ *
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int x, y, z;
+int (*f)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(100);
+}
+x = atoi(argv[1]);
+y = atoi(argv[3]);
+f = get_op_func(argv[2]);
+if (f == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+z = f(x, y);
+printf("%d\n", z);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *op_add - print sum of two values
+ *@a: first integer
+ *@b: second integer
+ *
+ *
+ *Return: sum
+ */
+int op_add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
+
+/**
+ *op_sub - print sum of two integerd
+ *@a: first integer
+ *@b: second integer
+ *
+ *
+ *Return: difference
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ *op_mul - print product of two values
+ *@a: first integer
+ *@b: second integer
+ *
+ *
+ *Return: product
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ *op_div - print divisor of two numbers
+ *@a: integer
+ *@b: integer
+ *
+ *
+ *Return: divisor
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error");
+exit(100);
+}
+return (a / b);
+}
+
+/**
+ *op_mod - print modulus of two numbers
+ *@a: integer
+ *@b: integer
+ *
+ *
+ *Return: modulus
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error");
+exit(100);
+}
+return (a % b);
+}
