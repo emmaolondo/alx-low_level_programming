be9c4df5785dffd3fc712abb6863ddb3aa09f450
emmaolondo <emmaolondo448@gmail.com>
2023-03-26 07:33:16 +0300

    ~print opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *opcode - peints rhe opcodes of ots main function
+ *@fun: a pointer to a function
+ *@byte: number of bytes to be printed
+ *
+ *
+ *Return: Void
+ */
+void opcode(char *fun, int byte)
+{
+int i;
+for (i = 0; i < byte ; i++)
+{
+printf("%2x", fun[i]);
+if (i < (byte -1))
+{
+printf(" ");
+}
+}
+printf("\n");
+}
+
+/**
+ *main - Main functio
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+  int num;
+  if (argc != 2)
+    {
+      printf("Error\n");
+      exit(1);
+    }
+  num = atoi(argv[1]);
+  if (num < 0)
+    {
+      printf("Error\n");
+      exit(2);
+    }
+  opcode((char *)&main ,num);
+  return (0);
+}
