646ebd56df39d557daf77fd3324c4bf2963307ab
emmaolondo <emmaolondo448@gmail.com>
2023-03-30 08:32:15 -0700

    add nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t i;/* counts number of nodes*/
+int i;/* counts number of nodes*/
 	i = 0;
 while (h)
 {
 if (h->str == NULL)
-	printf("[0](nil)\n");
+{
+printf("[0] (nil)\n");
+}
 else
-	printf("[%d][%s]\n", h->len, h->str);
+{
+printf("[%d][%s]\n", h->len, h->str);
+}
 i++;
 h = h->next;
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,9 @@
 int str_len(const char *str)
 {
 	int s_len;
+
 	s_len = 0;
+
 	while (str[s_len])
 	{/* get string length*/
 		s_len++;
@@ -29,27 +31,23 @@ int str_len(const char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head;
+
 	if (str == NULL)
 		return (NULL);
-	
 	new_head = malloc(sizeof(list_t));
 	if (new_head == NULL)
 	{
 		return (NULL);
 	}
-	
 	new_head->str = strdup(str);
-	if (new_head->str== NULL)
+	if (new_head->str == NULL)
 	{
 		free(new_head);
 		return (NULL);
 	}
-	
 	new_head->len = str_len(str);
 	new_head->next = NULL;
-	
 	new_head->next = *head;
 	*head = new_head;
-	
 	return (new_head);
 }
