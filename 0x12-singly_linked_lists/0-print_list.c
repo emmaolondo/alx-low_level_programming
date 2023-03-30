29e0c3e687178170cb729b4a5459a2465df22f48
emmaolondo <emmaolondo448@gmail.com>
2023-03-30 02:03:52 -0700

    print all elements in a link

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function to print  the number of elements
+ * @h: list_t
+ *
+ * Return: Number of nodes
+
+ */
+size_t print_list(const list_t *h)
+{
+size_t i;/* counts number of nodes*/
+	i = 0;
+while (h)
+{
+if (h->str == NULL)
+	printf("[0](nil)\n");
+else
+	printf("[%d][%s]\n", h->len, h->str);
+i++;
+h = h->next;
+}
+return(i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+Singly Linked List in C
+
+Leaning Objectives
+
+  *  When and why using linked lists vs arrays
+  *  How to build and use linked lists
+
