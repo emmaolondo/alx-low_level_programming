68a72a1b936e59e0d93db16a64fd8ff8c44d377b
emmaolondo <emmaolondo448@gmail.com>
2023-04-02 01:34:06 -0700

    string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_atoi - function
+ *@s: pointer string
+ *
+ *return: integer
+ */
+
+int _atoi(char *s)
+{
+unsigned int i;
+int n = 1;/* show the sign of value*/
+while (*s == ' ')
+{
+++s;
+}
+if (*s == '-')
+{
+++s;
+n = -1;/* change the sign to negative*/
+}
+else if (*s == '+')
+{
+++s;
+;
+}
+while (*s >= '0' && *s <= '9')
+{
+i = (i * 10) + (*s - '0');
+s++;
+}
+return (i * n);
+}
