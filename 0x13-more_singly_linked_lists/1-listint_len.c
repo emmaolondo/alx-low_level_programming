b60c361cff5323bf485705298b881ee3867d3490
emmaolondo <emmaolondo448@gmail.com>
2023-04-04 13:26:28 +0300

    print length of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ *listint_len - function that returns the number
+ *of elements in a linked
+ *@h: head of list
+ *
+ *Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+  int i;
+
+  i = 0;
+
+  while (h)
+    {
+      i++;
+      h = h->next;
+    }
+  return (i);
+}
