4a5c2ed1873b244955e0b8423fc3db53273cf0d9
emmaolondo <emmaolondo448@gmail.com>
2023-04-05 04:34:26 -0700

    betty rectification

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,7 +15,7 @@ typedef struct list_s
 char *str;
 int len;
 struct list_s *next;
-}list_t;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
