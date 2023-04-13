6bacef194180ca31db768dc9a4d6753e4983d55f
emmaolondo <emmaolondo448@gmail.com>
2023-04-13 07:27:51 -0700

    Reading a file using FILE I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rfile, wfile;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd != -1)
+	{
+		rfile = read(fd, buffer, letters);
+		if (rfile < 0)
+		{
+			free(buffer);
+			return (0);
+		}
+		wfile = write(STDOUT_FILENO, buffer, rfile);
+		if (wfile == -1)
+		{
+			free(buffer);
+			return (0);
+		}
+		close(fd);
+	}
+	else
+		return (0);
+
+	return (wfile);
+}
