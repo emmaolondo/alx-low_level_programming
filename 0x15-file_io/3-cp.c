a7f0ea155ef30e2fe13a4306574233e740a0cb3b
emmaolondo <emmaolondo448@gmail.com>
2023-04-14 15:12:23 -0700

    copy onr file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ */
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to, buffer[1024];
+	int fd, fd_copy, rfile, wfile;
+
+	if(argc != 3)
+	{
+		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd = open(file_from, O_RDWR);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd_copy = open(file_to, O_CREAT | O_RDWR | O_TRUNC | O_APPEND, 0664);
+
+	if (fd_copy == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit (99);
+	}
+	rfile = read(fd, buffer, 1024);
+	wfile = write(fd_copy, buffer, rfile);
+	if (wfile != rfile)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd_copy);
+		exit(100);
+	}
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_from);
+		exit(100);
+	}
+	if (close(fd_copy) == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_to);
+                exit(100);
+        }
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
