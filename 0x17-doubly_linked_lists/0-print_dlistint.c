b1b3fe31987bd3164d8c3a1038ce76ad70457005
emmaolondo <emmaolondo448@gmail.com>
2023-06-15 23:19:46 +0300

    print elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_dlistint - a function that prints all the elements of a list
+ * @h: head pointer
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		count++;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,8 @@
+** Doubly linked lists **
+
+Learning objectives
+
+   * What is a doubly linked list
+   * How to use doubly linked lists
+   * Start to look for the right source of information without too much help
+
