708dca9520164dc85c3cbc07ce113eb869a15a47
emmaolondo <emmaolondo448@gmail.com>
2023-06-19 03:39:22 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -9,15 +9,13 @@
 int main(void)
 {
     char *s = "First, solve problem. Then, write the code.";
-    char *k = "First, solve the problem. Then, write the code.";
-    char *f, *p;
+   /* char *k =;*/
+    char *f;
 
     f = _strchr(s, '\0');
-p = _strchr(k, ',');
     if (f != NULL)
     {
         printf("%s\n", f);
-	printf("%s\n", p);
     }
     return (0);
 }
