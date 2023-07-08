bfc30c0d2236aebd813f3d7ca2ef91e5e56d95c6
emmaolondo <emmaolondo448@gmail.com>
2023-07-08 05:44:19 +0300

    hash table using djb2 algorithm

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
-	unsigned int i = 0;
-	
+	unsigned long int i = 0;
+
 	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
 		return (NULL);
 
 	hash_t->size = size;
-	hash_t->array = malloc(sizeof(hash_table_t) * size);
+	hash_t->array = malloc(sizeof(hash_table_t *) * size);
 	if (hash_t->array == NULL)
 		return (NULL);
 	while (i < size)
