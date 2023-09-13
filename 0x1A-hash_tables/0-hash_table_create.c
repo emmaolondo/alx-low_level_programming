4240be1101546395f2b038229ec3f816e15892c7
root <emmaolondo448@gmail.com>
2023-09-13 15:23:32 -0700

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash;/**create a new pointer*/
+	hash_table_t *hash;
 	unsigned long int i = 0;
 
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
 		return (NULL);
 	hash->size = size;
-	hash->array = malloc(sizeof(hash_table_t) * size);
+	hash->array = malloc(sizeof(hash_table_t *) * size);
 	if (hash->array == NULL)
 		return (NULL);
 	while (i < size)
